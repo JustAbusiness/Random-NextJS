47756999cc564bbb3ada5748ce2cbb1ab91b2124
JustAbusinesse <huysanti123456@gmail.com>
2024-06-17 11:13:09 +0700

    push where

---
+++ JS/learn-next/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h2> Hi Next 14 ne haha</h2>
+    </main>
+  );
+}
